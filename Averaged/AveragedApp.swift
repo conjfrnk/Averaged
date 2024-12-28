a7c395d7c3e698478360268c2e5e358297ea1c03
Connor Frank <conjfrnk@gmail.com>
2024-12-27 21:55:49 -0500

    Initial Commit

---
+++ Averaged/AveragedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AveragedApp.swift
+//  Averaged
+//
+//  Created by Connor Frank on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AveragedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AveragedTests/AveragedTests.swift
@@ -0,0 +1,17 @@
+//
+//  AveragedTests.swift
+//  AveragedTests
+//
+//  Created by Connor Frank on 12/27/24.
+//
+
+import Testing
+@testable import Averaged
+
+struct AveragedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
